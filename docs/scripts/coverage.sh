16aac7e6ede4a738159722f32fa5ffade256c684
iCGY96 <gy.chen@foxmail.com>
2023-09-03 14:40:34 +0800

    update

--- docs/scripts/coverage.sh
+++
@@ -1,1 +0,0 @@
-coverage run --source ./autoagents -m pytest && coverage report -m && coverage html && open htmlcov/index.html
--- docs/scripts/get_all_classes_and_funcs.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-find autoagents | grep "\.py" | grep -Ev "(__init__|pyc)" | xargs grep -E "(^class| def )" 2>/dev/null | grep -v -E "(grep|tests|examples)"
